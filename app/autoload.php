d8145a06a1ff2bc91ff74ab76e51ab8266aecbd7
Maxime Bury <maxime.bury@gmail.com>
2012-05-08 11:28:38 +0200

    Gedmo OK. Dataset basique
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- app/autoload.php
+++ app/autoload.php
@@ -4,3 +4,8 @@
 Doctrine\Common\Annotations\AnnotationRegistry::registerFile(
     realpath(__DIR__.'/../') . '/vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php'
 );
+
+Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
+    'Gedmo\Mapping\Annotation',
+    realpath(__DIR__.'/../') . '/vendor/gedmo/doctrine-extensions/lib'
+);

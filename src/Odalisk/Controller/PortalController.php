3b82aa68092663b753e41f7397fe65b0d8b38d3e
Julien Sanchez <sumbobyboys@gmail.com>
2012-05-16 01:30:13 +0200

    Ajouts sur la vue des datasets
    Signed-off-by: Julien Sanchez <sumbobyboys@gmail.com>

--- src/Odalisk/Controller/PortalController.php
+++ src/Odalisk/Controller/PortalController.php
@@ -18,9 +18,31 @@ class PortalController extends Controller
     {
         // put action your code here
         
-        return array(
-            'name' => 'Julien Sanchez',
+        $repository = $this->getDoctrine()
+            ->getRepository('Odalisk\Entity\Portal');
+        $portals = $repository->findAll();
+        
+        return $this->render('App:Portal:index.html.twig', array(
             'maintenance_status' => $this->container->getParameter('app.maintenance'),
-        );
+            'page_number' => $page_number,
+            'portals' => $portals));
+    }
+    
+    /**
+     * details.
+     *
+     * @return array
+     */
+    public function details($portal_number, $_format)
+    {
+        // put action your code here
+        
+        $repository = $this->getDoctrine()
+            ->getRepository('Odalisk\Entity\Portal');
+        $portal = $repository->findById($portal_number);
+        $portal = $portal[0];
+        return $this->render('App:Portal:details.html.twig', array(
+            'maintenance_status' => $this->container->getParameter('app.maintenance'),
+            'portal' => $portal));
     }
 }

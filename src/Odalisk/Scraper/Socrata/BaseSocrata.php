8d904ad46b302f6354b6fa8936ef2bb5e243df54
Damien Launay <damien.launay.mail@gmail.com>
2012-05-17 13:42:45 +0200

    Pour le pull

--- src/Odalisk/Scraper/Socrata/BaseSocrata.php
+++ src/Odalisk/Scraper/Socrata/BaseSocrata.php
@@ -10,6 +10,11 @@ use Buzz\Message;
 
 abstract class BaseSocrata extends BasePlatform {
 
+	// The base url on which the datasets are listed.
+	private $datasets_list_url = 'https://opendata.socrata.com/browse?&page=';
+	// the number of datasets displayed on one page.
+	private $batch_size = 10;
+
 	public function __construct() {
 		$this->criteria = array(
 			'setName' => '//h2[@id="datasetName" and @class="clipText currentViewName"]'
@@ -31,12 +36,30 @@ abstract class BaseSocrata extends BasePlatform {
 	}
 
     public function getDatasetsUrls() {
-        // Get the paths
+		$urls    = array(); // the array we will return.
         $this->buzz->getClient()->setTimeout(30);
-        $response = $this->buzz->get(
-            $this->api_url,
-            $this->buzz_options
-        );
+
+        $response = $this->buzz->get($this->datasets_list_url.'1');
+		if($response->getStatusCode() != 200) {
+			echo('Impossible d\'obtenir la page : http://datamarket.com/data/list/?q=datatype:dataset');
+			return;
+		}
+
+		// We begin by fetching the number of datasets
+		$crawler = new Crawler($response->getContent());
+		$node = $crawler->filterXPath('//div[@class="resultCount"]');
+		if(preg_match("/([0-9]+)/", $node->text(), $match)) {
+			$datasets_number = (int) $match[1];
+			echo('Number of datasets of the platform : '.$datasets_number."\n");
+		}
+		// we now have the number of datasets of the portal.
+
+		// Since we already have the first page, let's parse it !
+		$ids  =
+		$crawler->filterXPath('//tr[@data-viewId]')->extract(array('data-viewId'));
+		// Add it to the urls array
+		$urls = array_merge($urls, $ids);
+
         if(200 == $response->getStatusCode()) {
             $data = json_decode($response->getContent());
             foreach($data as $dataset) {

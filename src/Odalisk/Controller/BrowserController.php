6036ddc5ff41cdbd91e064fa513db3064c8783d4
Julien Sanchez <sumbobyboys@gmail.com>
2012-05-20 01:56:51 +0200

    Remise en forme énorme du code avec de l'ajax dans tous les sens. Il ne reste plus qu'à faire l'affichage d'un dataset et on est bon !
    Signed-off-by: Julien Sanchez <sumbobyboys@gmail.com>

---
+++ src/Odalisk/Controller/BrowserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Odalisk\Controller;
+
+use Knp\Bundle\RadBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Portal controller.
+ */
+class BrowserController extends Controller
+{
+
+    /**
+     * index.
+     *
+     * @return array
+     */
+    public function index($_format)
+    {
+        
+
+        $repository = $this->getDoctrine()
+            ->getRepository('Odalisk\Entity\Portal');
+        $portals = $repository->findAll();
+        
+
+        return $this->render('App:Browser:index.html.twig', array(
+            'maintenance_status' => $this->container->getParameter('app.maintenance'),
+            'portals' => $portals));
+    }
+}

6440c3fca469eae1f78faaebc48688e7c1b36e35
Maxime Bury <maxime.bury@gmail.com>
2012-05-23 22:06:07 +0200

    Suppression des repository vides
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- src/Odalisk/Repository/DatasetRepository.php
+++ src/Odalisk/Repository/DatasetRepository.php
@@ -5,20 +5,6 @@ use Doctrine\ORM\EntityRepository;
 
 class DatasetRepository extends EntityRepository
 {
-    public function getFormats($datasetId)
-    {
-        $sth = $this->getEntityManager()
-            ->getConnection()
-            ->prepare('
-                    SELECT (formats.format)
-                    FROM formats JOIN dataset_format ON (id = format_id)
-                    WHERE dataset_id = :datasetId
-                    GROUP BY(format);
-                    ');
-        $sth = $sth->execute(array('datasetId' => $datasetId));
-        $res = $sth->fetchColumn();
-    }
-
     public function getDatasetsMatching($criterias, $page_index, $page_size) {
         $qb = $this->createQueryBuilder('d');
         // JOIN ... WITH IN (...)

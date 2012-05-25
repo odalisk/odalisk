ff0b4b24b318129a9b7329953d9c5e24805d4a22
Damien Launay <damien.launay.mail@gmail.com>
2012-05-25 12:29:19 +0200

    Config Montpellier

--- src/Odalisk/Scraper/LrnRln/BaseLrnRlnPortal.php
+++ src/Odalisk/Scraper/LrnRln/BaseLrnRlnPortal.php
@@ -18,19 +18,21 @@ abstract class BaseLrnRlnPortal extends BasePortal
     public function __construct()
     {
         $this->criteria = array(
-            'setName' => '//*div[@id='centre']/div[4]/ul/li'
-            , 'setSummary' => '//*div[@id='centre']/div[3]/p[3]'
-            , 'setReleasedOn' => '//*div[@id='centre']/div[4]/ul/li[3]'
-            , 'setLastUpdatedOn' => '//*div[@id='centre']/div[4]/ul/li[2]'
-            , 'setCategories' => '//*div[@id='centre']/div[4]/ul/li[7]'
-            , 'setRawLicense' => '//*div[@id='centre']/div[4]/ul/li[10]'
-            //, 'Tags' => '//div[@class="aboutDataset"]/div[4]/dl/dd[3]'
-            //, 'Permissions' => '//div[@class="aboutDataset"]/div[4]/dl/dd[2]'
-            , 'setProvider' => '//*div[@id='centre']/div[4]/ul/li[6]'
-            , 'setOwner' => '//*div[@id='centre']/div[4]/ul/li[5]'
-            , 'Time Period' => '//*div[@id='centre']/div[4]/ul/li[4]'
-            // , 'Frequency' => '//div[@class="aboutDataset"]/div[8]/dl/dd[3]/span'
-            // , 'Community Rating' => '//div[@class="aboutDataset"]/div[3]/dl/dd[1]/div'
+            'setName' => "//*div[@id='centre']/div[4]/ul/li",
+            /*
+            'setSummary' => "//*div[@id='centre']/div[3]/p[3]",
+            'setReleasedOn' => "//*div[@id='centre']/div[4]/ul/li[3]",
+            'setLastUpdatedOn' => "//*div[@id='centre']/div[4]/ul/li[2]",
+            'setCategories' => "//*div[@id='centre']/div[4]/ul/li[7]",
+            'setRawLicense' => "//*div[@id='centre']/div[4]/ul/li[10]",
+            //'Tags' => "//div[@class="aboutDataset"]/div[4]/dl/dd[3]",
+            //'Permissions' => "//div[@class="aboutDataset"]/div[4]/dl/dd[2]",
+            'setProvider' => "//*div[@id='centre']/div[4]/ul/li[6]",
+            'setOwner' => "//*div[@id='centre']/div[4]/ul/li[5]",
+            'Time Period' => "//*div[@id='centre']/div[4]/ul/li[4]",
+            //'Frequency' => // "//div[@class="aboutDataset"]/div[8]/dl/dd[3]/span",
+            //'Community Rating' => // "//div[@class="aboutDataset"]/div[3]/dl/dd[1]/div",
+            */
         );
     }
 
@@ -50,17 +52,20 @@ abstract class BaseLrnRlnPortal extends BasePortal
 
             // Now we fetch the datalists thanks to a <select> ;)
             $nodes = $crawler
-                ->filterXPath('//[@id="datalist"]//option[@value]')
+                ->filterXPath('//select[@id="datalist"]/option[@value]')
                 ->extract(array('value'))
                 ;
 
             // We construct the urls
             $base_url = $this->getBaseUrl();
             $this->urls = array_map(
-                    function($id) {
-                        return $base_url.$id;
+                    function($id) use($base_url) {
+                        return $base_url.substr($id, 3);
                     }
+                    , $nodes);
         }
+
+        $this->totalCount = count($this->urls);
 
         return $this->urls;
     }
--- src/Odalisk/Scraper/LrnRln/Montpellier/MontpellierPortal.php
+++ src/Odalisk/Scraper/LrnRln/Montpellier/MontpellierPortal.php
@@ -1,4 +1,3 @@
-
 <?php
 
 namespace Odalisk\Scraper\LrnRln\Montpellier;

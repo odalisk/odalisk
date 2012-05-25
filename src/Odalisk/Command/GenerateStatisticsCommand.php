3d428456052b48511a03f48d04bca260afee9c2b
Grolleau <grollof@hotmail.fr>
2012-05-25 17:07:00 +0200

    repartition update

--- src/Odalisk/Command/GenerateStatisticsCommand.php
+++ src/Odalisk/Command/GenerateStatisticsCommand.php
@@ -44,6 +44,8 @@ class GenerateStatisticsCommand extends BaseCommand
         $portals    = $portalRepo->findAll();
         $portalCriteriaRepo = $this->getEntityRepository('Odalisk\Entity\PortalCriteria');
 
+        
+        
         foreach($portals as $portal) {
 
             $general_value = 0;
@@ -80,7 +82,9 @@ class GenerateStatisticsCommand extends BaseCommand
             $this->em->persist($metric_general);
             $portal->setMetric($metric_general);
             $this->em->persist($portal);
+            
         }
+        
         $this->em->flush();
         $this->writeBlock($output, "[Statistics] The end !");
     }
